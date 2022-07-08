db877d6cd2d38c53f9a2f18fada415f45d5ec152
m94khalil <40716178+m94khalil@users.noreply.github.com>
2022-07-08 18:45:55 +0400

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -4,9 +4,9 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
-void	ft_putnbr_u_x(size_t n, size_t base, int flag);
-int		count_digits_u(size_t n, size_t base);
-int		count_digits_i(int n, size_t base);
+void	ft_putnbr_u_x(size_t nbr, size_t base, int flag);
+int		count_digits_u(size_t nbr, size_t base);
+int		count_digits_i(int nbr, size_t base);
 //int	count_digits(int n, size_t base, int flag);
 //char	*ptr_to_hex(uintptr_t address);
 
