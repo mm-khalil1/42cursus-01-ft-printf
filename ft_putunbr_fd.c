58483bd3436a83203b3008339324279f16f5dd69
m94khalil <40716178+m94khalil@users.noreply.github.com>
2022-07-08 18:49:42 +0400

    Update ft_putunbr_fd.c

--- ft_putunbr_fd.c
+++ ft_putunbr_fd.c
@@ -12,21 +12,21 @@
 
 #include "ft_printf.h"
 
-void	ft_putnbr_u_x(size_t n, size_t base, int flag)
+void	ft_putnbr_u_x(size_t nbr, size_t base, int flag)
 {
 	char	*ref;
 
 	ref = "0123456789abcdef";
-	if (n < base)
+	if (nbr < base)
 	{
 		if (flag == 'X' && base == 16)
-			ft_putchar_fd(ft_toupper(ref[n]), 1);
+			ft_putchar_fd(ft_toupper(ref[nbr]), 1);
 		else	
-			ft_putchar_fd(ref[n], 1);
+			ft_putchar_fd(ref[nbr], 1);
 	}
 	else
 	{
-		ft_putnbr_u_x(n / base, base, flag);
-		ft_putnbr_u_x(n % base, base, flag);
+		ft_putnbr_u_x(nbr / base, base, flag);
+		ft_putnbr_u_x(nbr % base, base, flag);
 	}
 }
